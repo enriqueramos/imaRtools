3177b558f214ea20aa30f09d2561809615fb05cf
Enrique Ramos <enrique756@gmail.com>
2015-08-03 13:49:31 -0500

    Some changes

--- R/read.template.R
+++ R/read.template.R
@@ -1,4 +1,4 @@
-read.template <- function(fileName, sheetName)
+read.template <- function(fileName, sheetName = NULL)
 {
   library(xlsx)
   #Loading required package: rJava
@@ -6,44 +6,46 @@ read.template <- function(fileName, sheetName)
   
   fileName = "Plantillas pelagicos.xlsx"
   
-  if(sheetName = "Anchoveta")
+  laboratorio = read.xlsx(file = fileName, sheetName = sheetName, rowIndex = 6, colIndex = 4, header = FALSE)
+  puerto = read.xlsx(file = fileName, sheetName = sheetName, rowIndex = 6, colIndex = 9, header = FALSE)
+  Date = read.xlsx(file = fileName, sheetName = sheetName, rowIndex = 7, colIndex = 13:15, header = FALSE)
+  d = Date[1]
+  m = Date[2]
+  y = Date[3]
+  
+  if(grepl("^[Aa]nchoveta", sheetName))
   {
+    tipo = "Anchoveta"
     tabla = read.xlsx(file = fileName, sheetName = sheetName, startRow = 21, endRow = 52, colIndex = 2:11)
-    laboratorio = read.xlsx(file = fileName, sheetName = sheetName, rowIndex = 6, colIndex = 4, header = FALSE)
-    Date = read.xlsx(file = fileName, sheetName = sheetName, rowIndex = 7, colIndex = 13:15, header = FALSE)
-    d = Date[1]
-    m = Date[2]
-    y = Date[3]
-    
-    header = read.xlsx(file = fileName, sheetName = sheetName, rowIndex = 20, colIndex = 3:11, header = FALSE, stringsAsFactors = FALSE)
+    header.tabla = read.xlsx(file = fileName, sheetName = sheetName, rowIndex = 20, colIndex = 3:11, header = FALSE, stringsAsFactors = FALSE)
     
     anchoveta = read.xlsx(file = fileName, sheetName = sheetName, startRow = 21, endRow = 52, colIndex = 2:5)
-    colnames(anchoveta)[2:4] = header[1:3]
+    colnames(anchoveta)[2:4] = header.tabla[1:3]
     samasa = read.xlsx(file = fileName, sheetName = sheetName, startRow = 21, endRow = 52, colIndex = c(2,6:8))
-    colnames(samasa)[2:4] = header[4:6]
+    colnames(samasa)[2:4] = header.tabla[4:6]
     sardina = read.xlsx(file = fileName, sheetName = sheetName, startRow = 21, endRow = 52, colIndex = 9:11)
-    colnames(sardina)[2:3] = header[8:9]
+    colnames(sardina)[2:3] = header.tabla[8:9]
+    new.tabla = list(anchoveta = anchoveta, samasa = samasa, sardina =  sardina)
+    
   }
   
-  if(sheetName = "Jurel Caballa")
+  if(grepl("^[Jj]urel [Cc]aballa", sheetName))
   {
+    tipo = "Jurel Caballa"
     tabla = read.xlsx(file = fileName, sheetName = sheetName, startRow = 21, endRow = 62, colIndex = 2:11)
-    laboratorio = read.xlsx(file = fileName, sheetName = sheetName, rowIndex = 6, colIndex = 4, header = FALSE)
-    puerto = read.xlsx(file = fileName, sheetName = sheetName, rowIndex = 6, colIndex = 9, header = FALSE)
-    Date = read.xlsx(file = fileName, sheetName = sheetName, rowIndex = 7, colIndex = 13:15, header = FALSE)
-    d = Date[1]
-    m = Date[2]
-    y = Date[3]
-    
-    header = read.xlsx(file = fileName, sheetName = sheetName, rowIndex = 20, colIndex = 3:14, header = FALSE, stringsAsFactors = FALSE)
+    header.tabla = read.xlsx(file = fileName, sheetName = sheetName, rowIndex = 20, colIndex = 3:14, header = FALSE, stringsAsFactors = FALSE)
     
     jurel = read.xlsx(file = fileName, sheetName = sheetName, startRow = 21, endRow = 62, colIndex = 2:5)
-    colnames(jurel)[2:4] = header[1:3]
+    colnames(jurel)[2:4] = header.tabla[1:3]
     caballa = read.xlsx(file = fileName, sheetName = sheetName, startRow = 21, endRow = 62, colIndex = c(2,6:8))
-    colnames(caballa)[2:4] = header[4:6]
+    colnames(caballa)[2:4] = header.tabla[4:6]
     bonito = read.xlsx(file = fileName, sheetName = sheetName, startRow = 21, endRow = 62, colIndex = c(2,9:11))
-    colnames(bonito)[2:4] = header[7:9]
+    colnames(bonito)[2:4] = header.tabla[7:9]
     jurelFino = read.xlsx(file = fileName, sheetName = sheetName, startRow = 21, endRow = 62, colIndex = c(2,12:14))
-    colnames(jurelFino)[2:4] = header[10:12]
+    colnames(jurelFino)[2:4] = header.tabla[10:12]
+    new.tabla = list(jurel = jurel, caballa = caballa, bonito = bonito, jurelFino = jurelFino)
+    
   }
+  
+  return(list(tipo = tipo, new.tabla = new.tabla, laboratorio = laboratorio, puerto = puerto, fecha = Date, d = d, m = m, y = y))
 }
